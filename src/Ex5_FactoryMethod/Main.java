d37789d6d20adc3c322ae83f88afc1bc1fa3901e
edufranco94 <eduardosantos9401@gmail.com>
2021-03-18 12:23:17 -0300

    correcao tratamento de erro e loop

--- src/Ex5_FactoryMethod/Main.java
+++ src/Ex5_FactoryMethod/Main.java
@@ -12,18 +12,17 @@ public class Main {
 
     public static int menu() {
         int numeroTanque;
-        do {
+        
             System.out.println("Escolha uma dos números abaixos e seleciona seu Canhão:");
             System.out.println("1) Megatron");
             System.out.println("2) Prime");
             System.out.println("3) Tracker");
             System.out.println("4) CrazyCannon");
-            System.out.println("5) Finalizar programa");
             numeroTanque = sc.nextInt();
 
             //Se algum valor digitado não for o que o programa espera, irá cair nessa laço:
-            while (numeroTanque < 1 || numeroTanque > 5) {
-                System.err.println("Valor inválido: insira valores entre 1 e 5");
+            while (numeroTanque < 1 || numeroTanque > 4) {
+                System.err.println("Valor inválido: insira valores entre 1 e 4");
                 numeroTanque = sc.nextInt();
             }
             
@@ -37,8 +36,6 @@ public class Main {
             tanque.atirar();
                        
             System.out.println("");
-        
-        } while (numeroTanque != 5);
         
         return numeroTanque;
     }
